89484d020a5ee3f73e25b23395d5f2a95e9d0d70
Agustín Paredes <paredes.skate@gmail.com>
2025-11-04 19:20:34 -0300

    Se agrega corrección en tables.sql

--- backend/tables.sql
+++ backend/tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE `medicos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --Tabla pacientes
-CREATE TABLE `pacientes` (
+CREATE TABLE `pacientes` (++++++++++++++++++++++
   `id_pacientes` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
   `apellido` varchar(45) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE `turnos` (
   `fecha` date NOT NULL,
   `hora` time NOT NULL,
   `estado` enum('pendiente','atendido','cancelado') NOT NULL,
-  `oservaciones` varchar(45) NOT NULL,
+  `observaciones` varchar(45) NOT NULL,
   `id_paciente` int NOT NULL,
   `id_medico` int NOT NULL,
   PRIMARY KEY (`id_turnos`),
@@ -33,7 +33,7 @@ CREATE TABLE `turnos` (
   KEY `fk_paciente_id_idx` (`id_paciente`),
   CONSTRAINT `fk_medico_id` FOREIGN KEY (`id_medico`) REFERENCES `medicos` (`id_medicos`),
   CONSTRAINT `fk_paciente_id` FOREIGN KEY (`id_paciente`) REFERENCES `pacientes` (`id_pacientes`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --Tabla usuarios
 CREATE TABLE `usuarios` (
