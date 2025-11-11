553d1e2d9dd76ff7afd4d5b8f2d0db4d08fbcf43
Agustín Paredes <paredes.skate@gmail.com>
2025-11-11 17:25:35 -0300

    Se agrega archivo index

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import { conectarDB } from "./db.js";
 import usuariosRouter from "./usuarios.js";
-import rolesRouter from "./roles.js";
-import usuariosRolesRouter from "./usuarios-roles.js";
+import medicosRouter from "./medicos.js";
+import pacientesRouter from "./pacientes.js";
+import turnosRouter from "./turnos.js";
+// import usuariosRolesRouter from "./usuarios-roles.js";
 import authRouter, { authConfig } from "./auth.js";
 
 conectarDB();
@@ -13,20 +15,19 @@ const port = 3000;
 
 // Para interpretar body como JSON
 app.use(express.json());
-
 // Habilito CORS
 app.use(cors());
-
 authConfig();
 
 app.get("/", (req, res) => {
   // Responder con string
-  res.send("Hola mundo!");
+  res.send("Gestión de pacientes, médicos y turnos funcionando en puerto " + port);
 });
 
 app.use("/usuarios", usuariosRouter);
-app.use("/roles", rolesRouter);
-app.use("/usuarios-roles", usuariosRolesRouter);
+app.use("/medicos", medicosRouter);
+app.use("/pacientes", pacientesRouter);
+app.use("/turnos", turnosRouter);
 app.use("/auth", authRouter);
 
 app.listen(port, () => {
